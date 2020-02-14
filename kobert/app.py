7bb4311a6ace827942d3695cf8d8ed3d585f8113
lhky78 <lhky0708@gmail.com>
2020-02-14 18:49:17 +0900

    kobert

---
+++ kobert/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request, render_template
+from flask_ngrok import run_with_ngrok
+from model import BertForEmotionClassification
+from datasets import get_pretrained_model, Datasets
+from pytorch_transformers.modeling_bert import BertConfig
+import numpy as np
+import torch
+
+pretrained_model_path = './best_model/best_model.bin'
+config_path = './best_model/bert_config.json'
+
+pretrained = torch.load(pretrained_model_path)
+bert_config = BertConfig(config_path)
+bert_config.num_labels = 7
+
+model = BertForEmotionClassification(bert_config)
+model.load_state_dict(pretrained, strict=False)
+model.eval()
+softmax = torch.nn.Softmax(dim=1)
+
+tokenizer, vocab = get_pretrained_model('etri')
+
+# '공포', '놀람', '분노', '슬픔', '중립', '행복', '혐오'
+# 'angry', 'surprise', 'angry', 'sadness', 'neutral', 'joy', 'disgust'
+label_list=np.array(['angry', 'surprise', 'angry', 'sadness', 'neutral', 'joy', 'disgust'])
+
+def get_prediction(sentence):
+    sentence = Datasets.normalize_string(sentence)
+    sentence = tokenizer.tokenize(sentence)
+    sentence = tokenizer.convert_tokens_to_ids(sentence)    
+    sentence = [vocab['[CLS]']] + sentence + [vocab['[SEP]']]
+    
+    output = model(torch.tensor(sentence).unsqueeze(0))
+    output_softmax = softmax(output)[0]
+    max_out = label_list[output_softmax.argmax()]
+    argidx = output_softmax.argsort(descending=True)
+    result = {label_list[i]: round(output_softmax[i].item(), 3) for i in range(len(label_list))}
+    sorted_result = {label_list[i]: round(output_softmax[i].item(), 3) for i in argidx}
+    return max_out, result, sorted_result
+
+app = Flask(__name__)
+app._static_folder = './static'
+run_with_ngrok(app)
+
+@app.route('/api', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        sentence = request.json['sentence']
+        max_out, result, sorted_result = get_prediction(sentence)
+        return jsonify({'input': sentence,
+                        'emotion': max_out,
+                        'output': result})
+
+@app.route('/')
+def index():
+    if request.args:
+        sentence = request.args['sentence']
+        max_out, result, sorted_result = get_prediction(sentence)
+        return render_template('index.html', sentence=sentence, result=result, sorted_result=sorted_result) 
+    else:
+        return render_template('index.html', result={}, sorted_result={})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+    
---
+++ kobert/app_test(formdata).py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify, request, render_template
+from flask_ngrok import run_with_ngrok
+from model import BertForEmotionClassification
+from datasets import get_pretrained_model, Datasets
+from pytorch_transformers.modeling_bert import BertConfig
+import numpy as np
+import torch
+
+pretrained_model_path = './best_model/best_model.bin'
+config_path = './best_model/bert_config.json'
+
+pretrained = torch.load(pretrained_model_path)
+bert_config = BertConfig(config_path)
+bert_config.num_labels = 7
+
+model = BertForEmotionClassification(bert_config)
+model.load_state_dict(pretrained, strict=False)
+model.eval()
+softmax = torch.nn.Softmax(dim=1)
+
+tokenizer, vocab = get_pretrained_model('etri')
+
+# '공포', '놀람', '분노', '슬픔', '중립', '행복', '혐오'
+# 'angry', 'surprise', 'angry', 'sadness', 'neutral', 'joy', 'disgust'
+obj = dict()
+emotion = ['scare', 'surprise', 'angry', 'sadness', 'neutral', 'joy', 'disgust']
+
+
+def get_prediction(sentence):
+    sentence = Datasets.normalize_string(sentence)
+    sentence = tokenizer.tokenize(sentence)
+    sentence = tokenizer.convert_tokens_to_ids(sentence)
+    sentence = [vocab['[CLS]']] + sentence + [vocab['[SEP]']]
+
+    output = model(torch.tensor(sentence).unsqueeze(0))
+    output_softmax = softmax(output)[0]
+    max_out = label_list[output_softmax.argmax()]
+    argidx = output_softmax.argsort(descending=True)
+    result = {label_list[i]: round(output_softmax[i].item(), 3) for i in range(len(label_list))}
+    sorted_result = {label_list[i]: round(output_softmax[i].item(), 3) for i in argidx}
+    return max_out, result, sorted_result
+
+
+app = Flask(__name__)
+# app._static_folder = './static'
+run_with_ngrok(app)  ##colab 환경에서 실행할 때
+
+
+@app.route('/')
+def test():
+    return render_template('post.html')
+
+
+@app.route('/post', methods=['POST'])
+def post():
+    sentence = request.form['input']
+    max_out, result, sorted_result = get_prediction(sentence)
+    obj['prediction'] = {
+        'emotion': max_out,
+        'data': result
+    }
+    return obj
+
+
+if __name__ == '__main__':
+    app.run()
