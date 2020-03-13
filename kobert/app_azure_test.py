5756ce716cae22efc171f495573ab5ea5cd9a652
HGForCode <lhky0708@gmail.com>
2020-03-13 17:03:51 +0900

    update app_azure_test.py

--- kobert/app_azure_test.py
+++ kobert/app_azure_test.py
@@ -17,7 +17,6 @@ import utils
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from util import constant
-from util import constant,functions as fc
 
 from azure.storage.file import FileService
 
